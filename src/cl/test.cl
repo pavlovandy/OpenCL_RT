673b40534ca3bc5653cffbf88c47bbdd1d17d024
apavlov <andrij0pawlov@gmail.com>
2019-09-15 18:04:07 +0300

    lil fixes and bugs

--- src/cl/test.cl
+++ src/cl/test.cl
@@ -39,7 +39,7 @@ double fresnel(double3 prim_ray, double3 normal, double n1, double reflective)
 	double ret = r0 + (1.0 - r0) * x * x * x * x * x;
  
 	ret = (reflective + (1.0 - reflective) * ret);
-	return ret;
+	return (ret);
 }
 
 double3	refract_ray(double3 prim_ray, double3 normal, double ior_new)
@@ -103,10 +103,13 @@ double3	calculate_light(__global t_scene *scene, double3 eye, \
 	double3	local_intensity;
 	double3	new_normal = normal;
 	double2 texture_space_coords;
+	double	light_len;
+	double3	pix_color;
+
 	if (fig.normal_map_no > -1 || fig.text_no > -1)
 		texture_space_coords = get_texture_space_coords(intersect_point, fig);
 	
-	if (fig.normal_map_no > -1) //bump mapping for sphere
+	if (fig.normal_map_no > -1)
 	{
 		double3	A = (double3)(1, 0, 0);
 		double3	t = normalize(cross(A, normal));
@@ -114,10 +117,10 @@ double3	calculate_light(__global t_scene *scene, double3 eye, \
 		double3	normal_from_map = uint_to_double3(get_texture_pixel(texture_space_coords, texture, txt_params[fig.normal_map_no], fig.normal_map_no));
 		double3 du = normal_from_map[0] / 255 * 2 - 1;
 		double3 dv = normal_from_map[1] / 255 * 2 - 1;
-		new_normal = normalize(normal - du * t + dv * b); //could be changes from + to -
+		new_normal = normalize(normal - du * t + dv * b);
 	}
 	
-	double3	pix_color;
+
 	if (fig.text_no > -1)
 	 	pix_color = uint_to_double3(get_texture_pixel(texture_space_coords, texture, txt_params[fig.text_no], fig.text_no));
 	else
@@ -128,9 +131,7 @@ double3	calculate_light(__global t_scene *scene, double3 eye, \
 	{
 		light = scene->light + i;
 		if (light->type_num == AMBIENT)
-		{
 			intensity += light->intensity;
-		}
 		else
 		{
 			if (light->type_num == POINT)
@@ -142,20 +143,19 @@ double3	calculate_light(__global t_scene *scene, double3 eye, \
 			local_intensity = get_intersity_after_shadow_rays(intersect_point, light_dir, scene, EPSILON, t_max, light);
 			if (length(local_intensity) < MINIMUM_INTENSITY)
 				continue ;
-
+			light_len = length(light_dir);
 			/*blicks*/
 			if (fig.specular > 0)
-			{						
+			{
 				reflect_ray = reflected_ray(new_normal, light_dir);
 				scalar = dot(reflect_ray, -dir);
 				if (scalar > 0)
-					intensity += local_intensity * pow(scalar / (length(-dir) * length(reflect_ray)), fig.specular); //d^2 where d is distance from light to dot
+					intensity += (1.0 / light_len * pow(scalar / (light_len * length(reflect_ray)), fig.specular) * local_intensity);
 			}
 			/*brightness*/
 			scalar = dot(new_normal, light_dir);
 			if (scalar > 0)
-				intensity += (local_intensity * scalar / (length(light_dir) * length(new_normal))); //d^2 where d is distance from light to dot
-			
+				intensity += (scalar / (light_len * light_len) * local_intensity);
 		}
 	}
 	return (pix_color * intensity);
@@ -202,7 +202,9 @@ double3		ray_trace(double3 eye, double3 dir, __global t_scene *scene, double min
 
 			//mix color and go deeper
 			local_color *= curr_node.part_of_primary_ray;
+
 			double fren = fresnel(curr_node.dir, normal, fig.ior, fig.reflective); //prart of reflected ray
+
 			if (fig.trans > 0 && count < tree_nodes && fren < 1)
 			{
 				tree[count].part_of_primary_ray = curr_node.part_of_primary_ray * (1 - fren) * fig.trans;
@@ -218,7 +220,7 @@ double3		ray_trace(double3 eye, double3 dir, __global t_scene *scene, double min
 			}
 			if (fig.reflective > 0 && count < tree_nodes)
 			{
-				tree[count].part_of_primary_ray = curr_node.part_of_primary_ray * fren * (1 - fig.trans);
+				tree[count].part_of_primary_ray = curr_node.part_of_primary_ray * (fig.reflective * (1 - fig.trans) + fig.trans * fren);
 				if (tree[count].part_of_primary_ray > MINIMUM_INTENSITY)
 				{
 					local_color *= (1 - fig.reflective);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: apavlov <apavlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 13:40:05 by apavlov           #+#    #+#             */
-/*   Updated: 2019/09/15 15:57:26 by apavlov          ###   ########.fr       */
+/*   Updated: 2019/09/15 18:00:19 by apavlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 void	make_little_default_scene(t_scene *scene)
 {
-	scene->count_obj = (cl_int)1;
-	scene->count_light = (cl_int)1;
+	scene->count_obj = (cl_int)3;
+	scene->count_light = (cl_int)2;
 	
 	scene->light[0].type_num = (cl_int)DIRECT;
-	scene->light[0].intensity = (cl_double3){{0.8, 0.8, 0.8}};
+	scene->light[0].intensity = (cl_double3){{0, 0, 0}};
 	scene->light[0].v = (cl_double3){{1, 4, 7}};
 
 	scene->light[1].type_num = (cl_int)POINT;
-	scene->light[1].intensity = (cl_double3){{0.8, 0.8, 0.8}};
-	scene->light[1].v = (cl_double3){{0, -5, 4}};
+	scene->light[1].intensity = (cl_double3){{25, 25, 25}};
+	scene->light[1].v = (cl_double3){{0, 1, 0}};
 
 	scene->light[2].type_num = (cl_int)POINT;
 	scene->light[2].intensity = (cl_double3){{0.8, 0.8, 0.8}};
@@ -41,23 +41,23 @@ void	make_little_default_scene(t_scene *scene)
 	// scene->obj[0].text_no = 0;
 
 	scene->obj[0].fig_type = (cl_int)SPHERE;
-	scene->obj[0].shape.sphere.cent = (cl_double3){{0, 0, 10}};
-	scene->obj[0].color = (cl_double3){{255, 255, 255}};
-	scene->obj[0].shape.sphere.radius = (cl_double)0.5;
+	scene->obj[0].shape.sphere.cent = (cl_double3){{2.5, 0, 10}};
+	scene->obj[0].color = (cl_double3){{0, 255, 0}};
+	scene->obj[0].shape.sphere.radius = (cl_double)1;
 	scene->obj[0].rotation = (cl_double3){{100 * M_PI / 180.0, 100 * M_PI / 180.0, 100 * M_PI / 180.0}};
 	scene->obj[0].rotation_martix = build_rotation_matrix_form_angles(scene->obj[0].rotation);
 	scene->obj[0].specular = (cl_int)-1;
 	scene->obj[0].reflective = (cl_double)0;
-	scene->obj[0].trans = (cl_double)0;
+	scene->obj[0].trans = (cl_double)0.9;
 	scene->obj[0].ior = (cl_double)1.4;
-	scene->obj[0].text_no = 0;
-	scene->obj[0].normal_map_no = 1;
+	scene->obj[0].text_no = -1;
+	scene->obj[0].normal_map_no = -1;
 	scene->obj[0].txt_offset = (cl_double2){{0, 0}};
 	scene->obj[0].txt_scale = (cl_double2){{1, 1}};
 
-	// scene->obj[1].fig_type = (cl_int)SPHERE;
-	// scene->obj[1].shape.sphere.cent = (cl_double3){{0, 0, 5}};
-	// scene->obj[1].shape.sphere.radius = (cl_double)1;
+	scene->obj[1].fig_type = (cl_int)SPHERE;
+	scene->obj[1].shape.sphere.cent = (cl_double3){{-3, 1, 5}};
+	scene->obj[1].shape.sphere.radius = (cl_double)2;
 
 	// scene->obj[1].fig_type = (cl_int)PLANE; //hasnt draw from one side
 	// scene->obj[1].shape.plane.dot = (cl_double3){{0, -1, 0}};
@@ -68,22 +68,37 @@ void	make_little_default_scene(t_scene *scene)
 	// scene->obj[1].shape.cylin.dir = (cl_double3){{0, 0, 1}};
 	// scene->obj[1].shape.cylin.radius = (cl_int)1.5;
 
-	scene->obj[1].fig_type = (cl_int)CONE;
-	scene->obj[1].shape.cone.vertex = (cl_double3){{0, -1, 0}};
-	scene->obj[1].shape.cone.dir = (cl_double3){{0, 0, 1}};
-	scene->obj[1].shape.cone.tangent = (cl_double)0.3;
-	//scene->obj[1].rotation = (cl_double3){{150.0 * M_PI / 180.0, 120.0 * M_PI / 180.0, 50.0 * M_PI / 180.0}};
-	scene->obj[1].rotation = (cl_double3){{0, 0, 0}};
+	// scene->obj[1].fig_type = (cl_int)CONE;
+	// scene->obj[1].shape.cone.vertex = (cl_double3){{0, -1, 0}};
+	// scene->obj[1].shape.cone.dir = (cl_double3){{0, 0, 1}};
+	// scene->obj[1].shape.cone.tangent = (cl_double)0.3;
+	scene->obj[1].rotation = (cl_double3){{150.0 * M_PI / 180.0, 120.0 * M_PI / 180.0, 50.0 * M_PI / 180.0}};
 	scene->obj[1].rotation_martix = build_rotation_matrix_form_angles(scene->obj[1].rotation);
 	scene->obj[1].color = (cl_double3){{255, 0, 0}};
 	scene->obj[1].specular = (cl_int)-1;
-	scene->obj[1].reflective = (cl_double)0;
-	scene->obj[1].trans = (cl_double)0.1;
+	scene->obj[1].reflective = (cl_double)0.8;
+	scene->obj[1].trans = (cl_double)0;
 	scene->obj[1].ior = (cl_double)1;
-	scene->obj[1].text_no = 2;
-	scene->obj[1].normal_map_no = 2;
+	scene->obj[1].text_no = -1;
+	scene->obj[1].normal_map_no = -1;
 	scene->obj[1].txt_offset = (cl_double2){{0, 0}};
 	scene->obj[1].txt_scale = (cl_double2){{1, 1}};
+
+	scene->obj[2].fig_type = (cl_int)PLANE;
+	scene->obj[2].shape.plane.dot = (cl_double3){{0, -1, 0}};
+	scene->obj[2].shape.plane.normal = (cl_double3){{0, 1, 0}};
+	scene->obj[2].rotation = (cl_double3){{150.0 * M_PI / 180.0, 120.0 * M_PI / 180.0, 50.0 * M_PI / 180.0}};
+	scene->obj[2].rotation_martix = build_rotation_matrix_form_angles(scene->obj[1].rotation);
+	scene->obj[2].color = (cl_double3){{255, 0, 0}};
+	scene->obj[2].specular = (cl_int)-1;
+	scene->obj[2].reflective = (cl_double)0.6;
+	scene->obj[2].trans = (cl_double)0.5;
+	scene->obj[2].ior = (cl_double)1;
+	scene->obj[2].text_no = 2;
+	scene->obj[2].normal_map_no = 2;
+	scene->obj[2].txt_offset = (cl_double2){{0, 0}};
+	scene->obj[2].txt_scale = (cl_double2){{1, 1}};
+	
 }
 
 int		main(int argc, char **argv)
