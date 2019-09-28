baadff4e6132753f5ffdbcab2ca8750516941de3
apavlov <andrij0pawlov@gmail.com>
2019-09-28 12:13:43 +0300

    Norms

--- src/button/init_but.c
+++ src/button/init_but.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_but.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: myuliia <myuliia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: apavlov <apavlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 21:53:56 by ozhyhadl          #+#    #+#             */
-/*   Updated: 2019/09/27 20:20:42 by myuliia          ###   ########.fr       */
+/*   Updated: 2019/09/28 12:09:20 by apavlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,14 @@ int		load_btt(const char *path, int type, t_button *butt, t_pov pov)
 
 	i = type;
 	if ((butt->image = IMG_Load(path)) == NULL)
-		return(error_message(RED"BUTT: invalid img"COLOR_OFF));
-	d = MIN(pov.w, pov.h); 
+		return (error_message(RED"BUTT: invalid img"COLOR_OFF));
+	d = MIN(pov.w, pov.h);
 	butt->type = type;
 	butt->available = 1;
 	butt->offset.x = 0.02 * d;
 	butt->offset.y = (0.02 * d) + (0.06 * d) * (i - 1);
-	butt->offset.w = 0.05  * d;
-	butt->offset.h = 0.05  * d;
+	butt->offset.w = 0.05 * d;
+	butt->offset.h = 0.05 * d;
 	return (0);
 }
 
@@ -47,7 +47,7 @@ int		init_but(t_rt *rt)
 {
 	if (load_btt("./buttons/d_a.png", SENT, &rt->butt[0], rt->pov))
 		return (1);
-	if (load_btt("./buttons/save.png", SAVE, &rt->butt[1],rt->pov))
+	if (load_btt("./buttons/save.png", SAVE, &rt->butt[1], rt->pov))
 		return (1);
 	if (load_btt("./buttons/screen.png", SCREEN, &rt->butt[2], rt->pov))
 		return (1);
